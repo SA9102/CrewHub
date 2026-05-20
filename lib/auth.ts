0acd098fadabdd5a4376b0646a9e2e71475c9573
ec23052 <ec23052@qmul.ac.uk>
2026-05-20 16:25:58 +0100

    changed bcrypt to bcryptjs

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import "server-only"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 export async function hashAndSaltPassword(password: string) {
   const hashed = await bcrypt.hash(password, 10)
